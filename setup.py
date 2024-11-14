3b3a7225f5ebaaf6fe053bf7472eb3c25cf96cbd
Kent Bull <kent@kentbull.com>
2024-11-14 23:34:30 +0700

    update version

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ from os.path import splitext
 
 from setuptools import find_packages, setup
 setup(
-    name='keri',
-    version='1.1.26',  # also change in src/keri/__init__.py
+    name='cit-keri',
+    version='1.1.24',  # also change in src/keri/__init__.py
     license='Apache Software License 2.0',
     description='Key Event Receipt Infrastructure',
     long_description="KERI Decentralized Key Management Infrastructure",
@@ -77,7 +77,7 @@ setup(
         'cbor2>=5.6.2',
         'multidict>=6.0.5',
         'ordered-set>=4.1.0',
-        'hio>=0.6.14',
+        'cit-hio>=0.6.14',
         'multicommand>=1.0.0',
         'jsonschema>=4.21.1',
         'falcon>=3.1.3',
