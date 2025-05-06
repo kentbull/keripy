e521a624011b9ceea8d88ef73c0d0769ab2b0c1c
Kent Bull <kent@kentbull.com>
2025-05-06 10:59:23 -0600

    cit-keri on gleif/keripy 1.2.8

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from os.path import splitext
 
 from setuptools import find_packages, setup
 setup(
-    name='keri',
+    name='cit-keri',
     version='1.2.8',  # also change in src/keri/__init__.py
     license='Apache Software License 2.0',
     description='Key Event Receipt Infrastructure',
